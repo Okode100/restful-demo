33fb31215de10d177e024425567d2f67a2710406
Okode100 <alfakings73@gmail.com>
2025-06-05 07:35:28 +0300

    create a router based on CRUD

--- app.js
+++ app.js
@@ -29,38 +29,37 @@ app.put('/app/course/:id', (req,res) =>{
     const course = courses.find(c => c.id === parseInt(req.params.id));
    if(!course) return res.status(404).send("The course with the given ID is not found");
 
-   const schema = {
-    name: Joi.string().min(3).required()
+  const {error} = validateCourse(req.body); //distr request.error 
+    if(error){
+    res.status(400).send(error.details[0].message);
 }
- const result = Joi.validate(req.body, schema);
- console.log(result);
 
-if(result.error){
-    res.status(400).send(result.error.details[0].message);
-}
 course.name = req.body.name;
 res.send(course);
 
 })
 
-app.post('/app/course', (req,resp) => {
+app.post('/app/course', (req, res) => {
+    // Validate the request body
+    const { error } = validateCourse(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+          
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+function validateCourse(course){
     const schema = {
         name: Joi.string().min(3).required()
     }
-     const result = Joi.validate(req.body, schema);
-     console.log(result);
-
-    if(result.error){
-        res.status(400).send(result.error.details[0].message);
-    }
-         
-    const course = {
-        id: courses.length + 1,
-        name: req.params.name
-    }
-    courses.push(course);
-    resp.send(course);
-})
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 
